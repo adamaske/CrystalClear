2aa716d649944ef6004e0a8e5af532160eeba405
Adam Aske <adamaske@live.no>
2021-05-15 15:05:27 +0200

    Project Created
    Unreal 4.26.2

---
+++ CrystalClear/Intermediate/Build/Win64/UE4Editor/Inc/CrystalClear/CrystalClearGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CRYSTALCLEAR_CrystalClearGameModeBase_generated_h
+#error "CrystalClearGameModeBase.generated.h already included, missing '#pragma once' in CrystalClearGameModeBase.h"
+#endif
+#define CRYSTALCLEAR_CrystalClearGameModeBase_generated_h
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_SPARSE_DATA
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_RPC_WRAPPERS
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACrystalClearGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrystalClearGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrystalClearGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrystalClear"), NO_API) \
+	DECLARE_SERIALIZER(ACrystalClearGameModeBase)
+
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACrystalClearGameModeBase(); \
+	friend struct Z_Construct_UClass_ACrystalClearGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACrystalClearGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CrystalClear"), NO_API) \
+	DECLARE_SERIALIZER(ACrystalClearGameModeBase)
+
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrystalClearGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrystalClearGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrystalClearGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrystalClearGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrystalClearGameModeBase(ACrystalClearGameModeBase&&); \
+	NO_API ACrystalClearGameModeBase(const ACrystalClearGameModeBase&); \
+public:
+
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrystalClearGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrystalClearGameModeBase(ACrystalClearGameModeBase&&); \
+	NO_API ACrystalClearGameModeBase(const ACrystalClearGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrystalClearGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrystalClearGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrystalClearGameModeBase)
+
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_12_PROLOG
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_SPARSE_DATA \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_RPC_WRAPPERS \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_INCLASS \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_SPARSE_DATA \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CRYSTALCLEAR_API UClass* StaticClass<class ACrystalClearGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CrystalClear_Source_CrystalClear_CrystalClearGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
