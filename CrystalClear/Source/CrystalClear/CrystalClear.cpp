c582d7fc07bbd18ff61224d50e4ba27f058154e2
Aske Aske <adamaske@live.no>
2021-12-12 21:29:02 +0100

    slettet alt

--- CrystalClear/Source/CrystalClear/CrystalClear.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CrystalClear.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrystalClear, "CrystalClear" );
