724eee3ab1f3f853138fff1b605ad6725e79aef2
Aske Aske <adamaske@live.no>
2022-09-19 17:44:37 +0200

    Sletta alt

--- CrystalClear/Source/CrystalClear/DialogueComponent.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DialogueComponent.h"
-
-void UDialogueComponent::Interact(APlayerUnit* player) {
-	GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Cyan, TEXT("Started dialouge"));
-}
--- CrystalClear/Source/CrystalClear/EnterCarComponent.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "EnterCarComponent.h"
-#include "PlayerUnit.h"
-void UEnterCarComponent::Interact(APlayerUnit* player) {
-
-}
